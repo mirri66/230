2d3fccf8e996089b863bd6bf9484240daa7c2bb0
Lazaro Clapp <lazaro.clapp@gmail.com>
2013-09-29 10:50:17 -0700

    Imports data

--- crawler/code_crawl.py
+++ crawler/code_crawl.py
@@ -7,19 +7,48 @@ def filesByPattern(directory, matchFunc):
     for f in filter(matchFunc, files):
       yield os.path.join(path, f)
 
-directory = sys.argv[1]
-javafiles = filesByPattern(directory, lambda fn: fn.endswith('.java'))
+def grab_imports(directory):
+	javafiles = filesByPattern(directory, lambda fn: fn.endswith('.java'))
+	imports = []
+	for fname in javafiles:
+	   with open(fname, 'r') as f:
+	      text = f.read()
+	      words = re.split(' |\t|\n|;',text)
+	      for i in range(0,len(words)):
+		word = words[i].strip()
+		if word == "import":
+		  val = words[i+1]
+		  if(val == "static"):
+		  	val = words[i+2]
+		  if(val not in imports):
+		  	imports.append(val)
 
-for fname in javafiles:
-   with open(fname, 'r') as f:
-      imports = []
-      text = f.read()
-      words = re.split(' |\t|\n|;',text)
-      for i in range(0,len(words)):
-	word = words[i].strip()
-	if word == "import":
-	  if(words[i+1] != "static"):
-	  	imports.append(words[i+1])
-	  else:
-		imports.append(words[i+2])
-      if(len(imports) > 0): print imports
+	imports.sort()
+	return imports
+
+def crawl_users_dir(usersdir):
+	s = "{"
+	usernames = os.listdir(usersdir)
+	for j in range(0, len(usernames)):
+		username = usernames[j]
+		s += "\"" + username + "\": ["
+		imports = grab_imports(usersdir + "/" + username)
+		for i in range(0, len(imports)):
+			imp = imports[i]
+			s += "\"" + imp + "\""
+			if(i != len(imports)-1): s += ", "
+		s += "]"
+		if(j != len(usernames)-1): s += ", "
+	s += "}"
+	return s
+
+usersdir = sys.argv[1]
+outs = crawl_users_dir(usersdir)
+with open(usersdir + "/" + "imports.data", "w") as outf:
+	outf.write(outs)
+
+#directory = sys.argv[1]
+#imports = grab_imports(directory)
+#with open(directory + "/" + "imports.info", "w") as outf:
+#	for imp in imports:
+#		outf.write(imp + "\n")
